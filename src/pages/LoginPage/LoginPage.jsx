b43818ccf8af6b66da5b461c05a38bc1d8fc1dec
BrySan20 <bryansanchez1680@gmail.com>
2025-11-19 15:54:35 -0600

    Prueba Blue-Green 1

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -128,7 +128,7 @@ const LoginPage = () => {
                     <CardContent>
                         <Box sx={{ display: "flex", flexDirection: "column", gap: isMobile ? 2 : 3 }}>
                             <Typography variant={isMobile ? "h6" : "h5"} sx={{ fontWeight: 'bold' }}>
-                                Login Version 2.0
+                                Login V2 - BlueGreen
                             </Typography>
 
                             <CustomTextField
