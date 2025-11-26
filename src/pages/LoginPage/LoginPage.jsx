cd6bed240d71c98ab7faaa810c561960414ad7e8
BrySan20 <bryansanchez1680@gmail.com>
2025-11-26 15:28:46 -0600

    Prueba Final: Switch automático a Blue

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -128,7 +128,7 @@ const LoginPage = () => {
                     <CardContent>
                         <Box sx={{ display: "flex", flexDirection: "column", gap: isMobile ? 2 : 3 }}>
                             <Typography variant={isMobile ? "h6" : "h5"} sx={{ fontWeight: 'bold' }}>
-                                Login V4 - AUTOMÁTICO
+                                Login V5 - FINAL
                             </Typography>
 
                             <CustomTextField
