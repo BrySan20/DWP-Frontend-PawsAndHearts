2ed032089e58df73518e04383d8882816277d1b2
BrySan20 <bryansanchez1680@gmail.com>
2025-11-26 16:04:34 -0600

    Prueba: Switch automático

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -128,7 +128,7 @@ const LoginPage = () => {
                     <CardContent>
                         <Box sx={{ display: "flex", flexDirection: "column", gap: isMobile ? 2 : 3 }}>
                             <Typography variant={isMobile ? "h6" : "h5"} sx={{ fontWeight: 'bold' }}>
-                                Login V6 - FINAL
+                                Login V7 - AUTOMATIZADO
                             </Typography>
 
                             <CustomTextField
