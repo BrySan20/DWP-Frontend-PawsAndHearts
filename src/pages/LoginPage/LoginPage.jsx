baac37cfe287217741b61b4d15c040a15f3f1563
BrySan20 <bryansanchez1680@gmail.com>
2025-11-26 16:16:53 -0600

    Prueba final de switcheo a Green

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -128,7 +128,7 @@ const LoginPage = () => {
                     <CardContent>
                         <Box sx={{ display: "flex", flexDirection: "column", gap: isMobile ? 2 : 3 }}>
                             <Typography variant={isMobile ? "h6" : "h5"} sx={{ fontWeight: 'bold' }}>
-                                Login V7 - AUTOMATIZADO
+                                Login V8 - FINAL TEST
                             </Typography>
 
                             <CustomTextField
